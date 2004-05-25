9b5b57deed0f8d07e8ecc70344ee3bbaa8694aa9
manu <manu>
2004-05-25 09:00:00 +0000

    typo

--- milter-greylist.c
+++ milter-greylist.c
@@ -1,4 +1,4 @@
-/* $Id: milter-greylist.c,v 1.86 2004/05/25 08:37:08 manu Exp $ */
+/* $Id: milter-greylist.c,v 1.87 2004/05/25 09:00:00 manu Exp $ */
 
 /*
  * Copyright (c) 2004 Emmanuel Dreyfus
@@ -34,7 +34,7 @@
 #ifdef HAVE_SYS_CDEFS_H
 #include <sys/cdefs.h>
 #ifdef __RCSID  
-__RCSID("$Id: milter-greylist.c,v 1.86 2004/05/25 08:37:08 manu Exp $");
+__RCSID("$Id: milter-greylist.c,v 1.87 2004/05/25 09:00:00 manu Exp $");
 #endif
 #endif
 
@@ -156,7 +156,7 @@ mlfi_envfrom(ctx, envfrom)
 
 	if ((priv->priv_queueid = smfi_getsymval(ctx, "{i}")) == NULL) {
 		syslog(LOG_DEBUG, "smfi_getsymval failed for {i}");
-		priv->priv_queueid = "(unkown id)";
+		priv->priv_queueid = "(unknown id)";
 	}
 
 	/*
