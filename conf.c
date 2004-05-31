4e69ba0a19d4f6aad4d9b2a04fb798e3f4bf37de
manu <manu>
2004-05-31 15:15:12 +0000

    Bug fix: on some platforms, reloading the config caused SIGSEGV It was caused by a stack overflow with pthread implementation that had a low limit on stack grow. We fix the problem by using a dedicated thread just for config reload (and we request a 2MB stck for that thread) Bump to 1.3.7

--- conf.c
+++ conf.c
@@ -1,4 +1,4 @@
-/* $Id: conf.c,v 1.20 2004/05/26 09:14:29 manu Exp $ */
+/* $Id: conf.c,v 1.21 2004/05/31 15:15:12 manu Exp $ */
 
 /*
  * Copyright (c) 2004 Emmanuel Dreyfus
@@ -34,7 +34,7 @@
 #ifdef HAVE_SYS_CDEFS_H
 #include <sys/cdefs.h>
 #ifdef __RCSID
-__RCSID("$Id: conf.c,v 1.20 2004/05/26 09:14:29 manu Exp $");
+__RCSID("$Id: conf.c,v 1.21 2004/05/31 15:15:12 manu Exp $");
 #endif
 #endif
 
@@ -83,6 +83,8 @@ void
 conf_load(void) 	/* exceptlist must be write-locked */
 {
 	FILE *stream;
+	pthread_t tid;
+	pthread_attr_t attr;
 
 	/*
 	 * Reset the configuration to its default 
@@ -100,8 +102,49 @@ conf_load(void) 	/* exceptlist must be write-locked */
 		return;
 	}
 
+	/*
+	 * On some platforms, the thread stack limit is too low and
+	 * conf_parse will get a SIGSEGV because it overflows the
+	 * stack.
+	 *
+	 * In order to fix this, we spawn a new thread just for 
+	 * parsing the config file, and we request a stack big 
+	 * enough to hold the parser data. 2 MB seems okay.
+	 */
 	conf_in = stream;
-	conf_parse();
+
+	if (pthread_attr_init(&attr) != 0) {
+		syslog(LOG_ERR, "pthread_attr_init failed: %s", 
+		    strerror(errno));
+		exit(EX_OSERR);
+	}
+
+	if (pthread_attr_setstacksize(&attr, 2 * 1024 * 1024) != 0) {
+		syslog(LOG_ERR, "pthread_attr_setstacksize failed: %s", 
+		    strerror(errno));
+		exit(EX_OSERR);
+	}
+
+	if (pthread_create(&tid, &attr, 
+	    (void *(*)(void *))conf_parse, NULL) != 0) {
+		syslog(LOG_ERR, "pthread_create failed: %s", 
+		    strerror(errno));
+		exit(EX_OSERR);
+	}
+
+	if (pthread_join(tid, NULL) != 0) {
+		syslog(LOG_ERR, "pthread_join failed: %s",
+		    strerror(errno));
+		exit(EX_OSERR);
+	}
+
+	if (pthread_attr_destroy(&attr) != 0) {
+		syslog(LOG_ERR, "pthread_attr_destroy failed: %s",
+		    strerror(errno));
+		exit(EX_OSERR);
+	}
+		
+
 	fclose(stream);
 
 	(void)gettimeofday(&conffile_modified, NULL);
